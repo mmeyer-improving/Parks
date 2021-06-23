8c2c7b7e079eadb7c944cb249e1bcb57ff44edb2
Michael Meyer <michael.meyer@improving.com>
2021-06-23 08:20:22 -0500

    Initial commit so I can send to Eric

---
+++ Parks/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Parks.Models;
+
+namespace Parks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ParkData()
+        {
+            var path = Request.Path;
+            var query = Request.Query;
+            var life = 42;
+            ViewBag.Data = await client.GetAsync(Request.Path);
+            return View("ParkData");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
